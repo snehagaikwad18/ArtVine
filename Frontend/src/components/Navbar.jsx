c264bba27f0cfd6f434194d4a4423dcbc88d9cb8
snehagaikwad <snehagaikwad18@gmail.com>
2024-12-22 16:01:57 +0530

    initial commit

---
+++ Frontend/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div>
+            <div className="flex flex-row gap-5 font-semibold text-[17px]">
+                <h1>Biography</h1>
+                <h1>Works</h1>
+                <h1>Custom Art</h1>
+                <h1>Testinomials</h1>
+                <h1>Track Order</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
---
+++ Frontend/src/pages/LandingPage.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import HeroSection from '../components/HeroSection'
+
+const LandingPage = () => {
+    return (
+        <div className='playfair-display-regular' >
+            <HeroSection />
+                <h1>biography</h1>
+                <h1>works</h1>
+                <h1>custome art(exhibition)</h1>
+                <h1>testinomials</h1>
+                <h1>footer</h1>
+
+        </div>
+    )
+}
+
+export default LandingPage
