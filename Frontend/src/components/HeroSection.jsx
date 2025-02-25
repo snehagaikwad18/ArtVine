5cdf4e3bc3cafd740c2b3ae839a97d9d59e53112
snehagaikwad <snehagaikwad18@gmail.com>
2025-02-25 17:12:45 +0530

    fix(landingPage) import ArtVine.jsx

--- Frontend/src/components/HeroSection.jsx
+++ Frontend/src/components/HeroSection.jsx
@@ -5,7 +5,7 @@ const HeroSection = () => {
         <div className='bg-[url("/homeBg.jpg")] bg-cover bg-center h-screen   '>
             <Navbar />
             <div className="px-[140px] flex flex-col  justify-center h-2/3 pt-[59px] ">
-                <h1 className='text-[#ffffff]  text-[90px] leading-[95px] px-6 '>Sneha <br />Gaikwad</h1>
+                <h1 className='text-[#ffffff]  text-[90px] leading-[95px] px-6 '>Sneha <br />Gaikwad BE/B/74 </h1>
                 <br />
                 <p className='text-[#9EC4BD] text-[20px] text-end w-[490px]   pr-[50px] cursor-pointer'>Read more -</p>
             </div>
